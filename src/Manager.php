3385e0cb75b42c60acdde35e8e9d5c1e6e39beb1
Orest Budzhak <doom4eg@gmail.com>
2015-11-02 10:12:01 +0200

    SD-1364 remove subscription canceling on device delete method

--- src/Manager.php
+++ src/Manager.php
@@ -883,8 +883,6 @@ class Manager
                 
         $deviceRecord->setDeleted()
                 ->save();
-
-        $this->closeDeviceLicenses($deviceId, true, $actorAdminId);
         
         $eventManager = EventManager::getInstance();
         $eventManager->emit('device-deleted', array(
